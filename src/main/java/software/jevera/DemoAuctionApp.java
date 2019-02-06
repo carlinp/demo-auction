61a910ad2a3ef2694738e588ad7e04522dbdcadd
ssenko <sergeysenja1992@gmail.com>
2019-02-06 21:56:39 +0200

    change state mahine

--- src/main/java/software/jevera/DemoAuctionApp.java
+++ src/main/java/software/jevera/DemoAuctionApp.java
@@ -1,11 +1,12 @@
 package software.jevera;
 
-import software.jevera.service.UserService;
+import software.jevera.configuration.ApplicationFactory;
+import software.jevera.domain.UserDto;
 
 public class DemoAuctionApp {
 
     public static void main(String[] args) {
-
+        ApplicationFactory.userService.registerUser(new UserDto());
     }
 
 }
--- src/main/java/software/jevera/domain/UserDto.java
+++ src/main/java/software/jevera/domain/UserDto.java
@@ -4,6 +4,14 @@ public class UserDto {
     private String login;
     private String password;
 
+    public UserDto() {
+    }
+
+    public UserDto(String login, String password) {
+        this.login = login;
+        this.password = password;
+    }
+
     public String getLogin() {
         return login;
     }
