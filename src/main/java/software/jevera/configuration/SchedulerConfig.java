ead488289469b8c129b603db15a85fb44fcad801
ssenko <sergeysenja1992@gmail.com>
2019-03-09 13:36:40 +0200

    add logs

--- src/main/java/software/jevera/configuration/SchedulerConfig.java
+++ src/main/java/software/jevera/configuration/SchedulerConfig.java
@@ -1,18 +1,20 @@
 package software.jevera.configuration;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.TaskScheduler;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 
+@Slf4j
 @Configuration
 public class SchedulerConfig {
 
     @Bean
     public TaskScheduler taskScheduler(@Value("${scheduler.thread.count}") int threadCount) {
         ThreadPoolTaskScheduler taskScheduler = new ThreadPoolTaskScheduler();
-        System.out.println(threadCount);
+        log.info("Thread count {}", threadCount);
         taskScheduler.setPoolSize(threadCount);
         return taskScheduler;
     }
