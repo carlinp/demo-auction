f80147a45aa1fb2a3aa0da0940e7595c86f91114
ssenko <sergeysenja1992@gmail.com>
2019-03-24 18:58:21 +0200

    add spring test

--- src/test/java/software/jevera/service/ProductServiceTest.java
+++ src/test/java/software/jevera/service/ProductServiceTest.java
@@ -1,36 +1,59 @@
 package software.jevera.service;
 
-import static org.junit.Assert.*;
+import static java.util.Optional.of;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.ArgumentMatchers.refEq;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static software.jevera.service.product.ProductStateEnum.NEW;
+import static software.jevera.service.product.ProductStateEnum.PUBLISHED;
 
+import java.time.Instant;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import software.jevera.dao.ProductRepository;
+import software.jevera.domain.Product;
+import software.jevera.service.product.Archived;
+import software.jevera.service.product.Deleted;
+import software.jevera.service.product.Finished;
+import software.jevera.service.product.New;
+import software.jevera.service.product.ProductState;
+import software.jevera.service.product.Published;
+import software.jevera.service.product.StateMachine;
 
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes = {ProductService.class, StateMachine.class, Archived.class,
+                                 Deleted.class, Finished.class, New.class, Published.class})
 public class ProductServiceTest {
 
-    @Test
-    public void createProduct() {
-    }
+    public static final long PRODUCT_ID = 123;
 
-    @Test
-    public void getAllProducts() {
-    }
+    @Autowired
+    private ProductService productService;
 
-    @Test
-    public void getUserProducts() {
-    }
+    @MockBean
+    private ProductRepository productRepository;
+
+    @MockBean
+    private ScheduleExecutor scheduleExecutor;
 
     @Test
     public void publish() {
-    }
-
-    @Test
-    public void delete() {
-    }
-
-    @Test
-    public void finish() {
-    }
-
-    @Test
-    public void addBid() {
+        Product product = new Product();
+        product.setId(PRODUCT_ID);
+        product.setStatus(NEW);
+        when(productRepository.findById(PRODUCT_ID)).thenReturn(of(product));
+        productService.publish(PRODUCT_ID);
+        Product publishedProduct = new Product();
+        publishedProduct.setId(PRODUCT_ID);
+        publishedProduct.setStatus(PUBLISHED);
+        publishedProduct.setFinishDate(product.getFinishDate());
+        verify(productRepository).save(refEq(publishedProduct));
+        verify(scheduleExecutor).scheduleFinish(eq(PRODUCT_ID), eq(product.getFinishDate()), any());
     }
 }
